3499eac6ad7b367e2586994af617dfe015cbe051
João Vitor Antoniassi Segantin <segantin.jv@gmail.com>
2019-12-10 08:45:57 -0300

    Clean camera code

--- camera.h
+++ camera.h
@@ -5,16 +5,6 @@
 #include "general-structures.h"
 #include "snowman.h"
 
-namespace CAMERA
-{
-    enum
-    {
-        TOP_IS_X,
-        TOP_IS_Y,
-        TOP_IS_Z
-    };
-}
-
 class Camera
 {
     public:
@@ -24,10 +14,8 @@ class Camera
     GLfloat angle;
     GLfloat aspect;
 
-    // Defaults to TOP_IS_Y
     Camera()
     {
-        this->topIs = CAMERA::TOP_IS_Y;
         this->position.x = 0;
         this->position.y = 0;
         this->position.z = 10;
@@ -37,30 +25,10 @@ class Camera
 
     void look(void)
     {
-        switch (this->topIs)
-        {
-        case CAMERA::TOP_IS_X:
-            gluLookAt(
-                this->position.x, this->position.y, this->position.x,
-                this->position.x, this->position.y, this->position.z,
-                1, 0, 0);
-            break;
-        
-        case CAMERA::TOP_IS_Z:
-            gluLookAt(
-                this->position.x, this->position.y, this->position.x,
-                this->position.x, this->position.y, this->position.z,
-                0, 0, 1);
-            break;
-
-        case CAMERA::TOP_IS_Y:
-        default:
-            gluLookAt(
-                this->position.x, this->position.y, this->position.x,
-                this->position.x, this->position.y, this->position.z,
-                0, 1, 0);
-            break;
-        }
+        gluLookAt(
+            this->position.x, this->position.y, this->position.x,
+            this->position.x, this->position.y, this->position.z,
+            0, 1, 0);
     }
 
     void updateAspect(GLfloat width, GLfloat height)
